972b61e0b5621d5205c39552b056a8b36e031e3c
Eric R. Hoglund <erh3cq@virginia.edu>
2024-02-06 15:55:24 -0500

    io_continued

--- hoglundTools/io.py
+++ hoglundTools/io.py
@@ -4,6 +4,7 @@ import json
 import sys
 import os
 import numpy as np
+import dask.array as da
 import h5py
 import json
 from numpy.typing import NDArray
@@ -36,11 +37,18 @@ class swift_json_reader:
 
         #Instrument metadata
         self.beam_energy = self.meta['metadata']['instrument'].get('high_tension')/1E3
-        self.exposure = self.meta['metadata']['hardware_source'].get('exposure')
+        self.aberrations = self.read_abberations()
+
+        #Scan metadata
         if self.is_scan:
             self.scan_rotation = self.meta['metadata']['scan'].get('rotation_deg')
             self.dwell_time = self.meta['metadata']['scan']['scan_device_parameters'].get('pixel_time_us') * 1E-6
-        self.aberrations = self.read_abberations()
+        
+        #Detector metadata
+        self.exposure = self.meta['metadata']['hardware_source'].get('exposure')
+        self.binning = self.meta['properties'].get('binning')
+        self.flip_x = self.meta['properties'].get('is_flipped_horizontally')
+        
 
     def read_signal_type(self):
         signal_type = self.meta['properties'].get('signal_type') or \
@@ -69,7 +77,7 @@ class swift_json_reader:
             self.axes_qspace_dims = [-2, -1]
             for i in self.axes_qspace_dims:
                 self.axes[i]['scale'] = 1
-                self.axes[i]['offset'] = self.data_shape[i]/2
+                self.axes[i]['offset'] = -self.data_shape[i]/2
                 self.axes[i]['units'] = 'px'
 
     def read_abberations(self):
@@ -106,6 +114,13 @@ def swift_meta_to_hs_dict(swift_metadata:object, signal_type:str=None) -> dict:
         Dictionary in the format of hyperspy metadata.
     """
     if signal_type is None: signal_type = swift_metadata.signal_type
+    if signal_type is None:
+        if swift_metadata.sig_dim == 1:
+            signal_type = swift_metadata.signal_type = 'Signal1D'
+        elif swift_metadata.sig_dim == 2:
+            signal_type = swift_metadata.signal_type = 'Signal2D'
+        else:
+            print('HyperSpy signal type could not be infered. Please set it manually.')
 
     meta_dict = {}
     meta_dict['General'] = dict(title=swift_metadata.title)
@@ -150,8 +165,13 @@ def load_swift_to_hs(file:str, signal_type:str=None, lazy:bool=False, **kwargs) 
 
     meta = swift_json_reader(file, signal_type=signal_type)
 
-    mmap = 'c' if kwargs.get('lazy') else None
-    data = np.load(file+'.npy', mmap_mode=mmap)
+    #mmap = 'c' if kwargs.get('lazy') else None
+    #data = np.load(file+'.npy', mmap_mode=mmap)
+    if lazy:
+        #data = da.from_array(np.memmap(file+'.npy', mode='r'))
+        data = da.from_array(np.load(file+'.npy', mmap_mode='c'))
+    else:
+        data = np.load(file+'.npy')
 
     if meta.sig_dim == 1:
         Signal = Signal1D
@@ -163,6 +183,14 @@ def load_swift_to_hs(file:str, signal_type:str=None, lazy:bool=False, **kwargs) 
     sig = Signal(data, axes=meta.axes,
                  metadata=swift_meta_to_hs_dict(meta),
                  original_metadata=meta.meta)
+    if lazy:
+        sig = sig.as_lazy()
+        
+    if signal_type == 'diffraction':
+        sig.set_signal_type('electron_diffraction')
+    elif meta.signal_type == 'EELS' or signal_type == 'EELS':
+        sig.set_signal_type('EELS')
+    
     return sig
 
 def load_swift_to_hdf5(file:str, signal_type:str=None, lazy:bool=False, **kwargs) -> object:
@@ -194,32 +222,127 @@ def load_swift_to_hdf5(file:str, signal_type:str=None, lazy:bool=False, **kwargs
     return f
 
 
-def convert_swift_to_py4DSTEM(file:str, lazy:bool=False, **kwargs) -> object:
+# def convert_swift_to_py4DSTEM(file:str, lazy:bool=False, **kwargs) -> object:
+#     meta = swift_json_reader(file, signal_type='diffraction')
+    
+#     mmap = 'c' if kwargs.get('lazy') else None
+#     data = np.load(file+'.npy', mmap_mode=mmap)
+#     if meta.is_series:
+#         raise('Reading of series are not currently implimented. The data must be a four-dimenstional 4D-STEM scan.')
+#     print(f'Creating {file}.hdf5', end='...')
+#     f = h5py.File(file+".hdf5", "w")
+    
+#     ds = f.create_group("Experiments")
+#     ds.attrs['emd_group_type'] = "root"
+#     ds.attrs['version_major'] = "0"
+#     ds.attrs['version_minor'] = "13"
+
+#     ds = ds.create_group("datacube")
+#     ds.attrs['emd_group_type'] = 1
+#     if len(meta.axes) == 2:
+#         ds.attrs['py4dstem_class'] = "DiffractionSlice"
+#     else:
+#         ds.attrs['py4dstem_class'] = "DataCube"
+
+#     dc = ds.create_dataset("data", data=data)
+#     dc.attrs['units'] = "pixel intensity"
+    
+#     axis_names = {'x':'Rx', 'y':'Ry', 'qx':'Qx', 'qy':'Qy'}
+#     for i,ax in enumerate(meta.axes):
+#         dim = ds.create_dataset(f"dim{i}", data=np.arange(ax['size']))
+#         dim.attrs['name'] = axis_names[ax['name']]
+#         dim.attrs['units'] = ax['units']
+    
+#     cal = ds.create_group("calibration")
+#     cal.attrs['emd_group_type'] = 0
+#     cal.attrs['py4dstem_class'] = "Calibration"
+#     if meta.axes[0]['name'] in 'xyz':
+#         cal.create_dataset('R_pixel_size', data=meta.axes[0]['scale'])
+#         cal.create_dataset('R_pixel_units', data=meta.axes[0]['units'])
+#     cal.create_dataset('Q_pixel_size', data=meta.axes[-1]['scale'])
+#     cal.create_dataset('Q_pixel_units', data=meta.axes[-1]['units'])
+#     cal.create_dataset('qx0_mean', data=-meta.axes[-2]['offset'])
+#     cal.create_dataset('qy0_mean', data=-meta.axes[-1]['offset'])
+#     for v in cal.values(): 
+#         if h5py.check_string_dtype(v.dtype):
+#             #v.attrs["type"] = 'string'.encode("latin-1")
+#             v.attrs.create("type", 'string'.encode("latin-1"))
+#         else:
+#             #v.attrs["type"] = "number".encode("latin-1")
+#             v.attrs.create("type", 'number'.encode("latin-1"))
+    
+#     print('Created.')
+#     f.close()
+
+def convert_swift_to_py4DSTEM(file:str, lazy:bool=False, verbose=False, **kwargs) -> object:
+    def add_dataset_wAttrs(add_to, name:str, data, attributes:dict):
+        set = add_to.create_dataset(name, data=data)
+        for k,v in attributes.items():
+            set.attrs.create(k, v)
+        return set
+    def add_group_wAttrs(add_to, name:str, attributes:dict):
+        grp = add_to.create_group(name)
+        for k,v in attributes.items():
+            grp.attrs.create(k, v)
+        return grp
+
     meta = swift_json_reader(file, signal_type='diffraction')
     
     mmap = 'c' if kwargs.get('lazy') else None
     data = np.load(file+'.npy', mmap_mode=mmap)
-    if meta.is_series:
-        raise('Reading of series are not currently implimented. The data must be a four-dimenstional 4D-STEM scan.')
+    if meta.is_series: raise('Reading of series are not currently implimented. The data must be a four-dimenstional 4D-STEM scan.')
+    end = '...' if verbose else '\n'
     print(f'Creating {file}.hdf5', end='...')
-    f = h5py.File(file+".hdf5", "w")
-    ds = f.create_group("Experiments").create_group("datacube")
-    ds.create_dataset("data", data=data)
     
+    f = h5py.File(file+".hdf5", "w")
+    f.attrs['authoring_program'] = 'hoglundTools'
+    f.attrs['authoring_user'] = ""
+    f.attrs['emd_group_type'] = "file"
+    f.attrs['version_major'] = 1
+    f.attrs['version_minor'] = 0
+    
+    root = add_group_wAttrs(f, 'Experiments',
+                           {'emd_group_type': "root", 'python_class': "Root"})
+    ds = add_group_wAttrs(root, "aquisition", {'emd_group_type': "array"})
+    if len(meta.axes) == 2:
+        ds.attrs['python_class'] = "DiffractionSlice"
+    else:
+        ds.attrs['python_class'] = "DataCube"
+
+    if meta.flip_x:
+        if verbose: print('Detector flip_x flagged True. Reversing the qx axis.')
+    else:
+        if verbose: print('Detector flip_x flagged False. Not reversing the qx axis.')
+        data = data[...,::-1]
+        #add_dataset_wAttrs(cal, 'QR_flip', -meta.axes[-1]['offset'], {"type": 'bool'})
+    add_dataset_wAttrs(ds, "data", data,
+                       {'units': "pixel intensity"})
     
     axis_names = {'x':'Rx', 'y':'Ry', 'qx':'Qx', 'qy':'Qy'}
     for i,ax in enumerate(meta.axes):
-        dim = ds.create_dataset(f"dim{i}", data=np.arange(ax['size']))
-        dim.attrs['name'] = axis_names[ax['name']]
-        dim.attrs['units'] = ax['units']
-    cal = ds.create_group("calibration")
+        add_dataset_wAttrs(ds, f"dim{i}", np.arange(ax['size']),
+                            {'name':axis_names[ax['name']], 'units':ax['units']})
+    
+    met = add_group_wAttrs(root, "metadatabundle", {'emd_group_type': "metadatabundle"})
+
+    cal = add_group_wAttrs(met, "calibration",
+                            {'emd_group_type': "metadata", 'python_class': "Calibration"})
+
+    #add_dataset_wAttrs(cal, '_root_treepath', b'', {"type":'string'})
+    grp = add_group_wAttrs(cal, '_target_paths', {"type":'list_of_strings', 'length':1})
+    grp.create_dataset('0', data=b'/aquisition')
+
     if meta.axes[0]['name'] in 'xyz':
-        cal.create_dataset('R_pixel_size', data=meta.axes[0]['scale'])
-        cal.create_dataset('R_pixel_units', data=meta.axes[0]['units'])
-    cal.create_dataset('Q_pixel_size', data=meta.axes[-1]['scale'])
-    cal.create_dataset('Q_pixel_units', data=meta.axes[-1]['units'])
-    cal.create_dataset('qx0_mean', data=-meta.axes[-2]['offset'])
-    cal.create_dataset('qy0_mean', data=-meta.axes[-1]['offset'])
+        add_dataset_wAttrs(cal, 'R_pixel_size', meta.axes[0]['scale'], {"type":'number'})
+        add_dataset_wAttrs(cal, 'R_pixel_units', meta.axes[0]['units'], {"type": 'string'})
+    add_dataset_wAttrs(cal, 'Q_pixel_size', meta.axes[-1]['scale'], {"type": 'number'})
+    add_dataset_wAttrs(cal, 'Q_pixel_units', meta.axes[-1]['units'], {"type": 'string'})
+    add_dataset_wAttrs(cal, 'qx0_mean', -meta.axes[-2]['offset'], {"type": 'number'})
+    add_dataset_wAttrs(cal, 'qy0_mean', -meta.axes[-1]['offset'], {"type": 'number'})
+    if meta.is_scan:
+        add_dataset_wAttrs(cal, 'QR_rotation', np.deg2rad(meta.scan_rotation), {"type": 'number'})
+        add_dataset_wAttrs(cal, 'QR_rotation_degrees', meta.scan_rotation, {"type": 'number'})
+    
     
     print('Created.')
     f.close()
